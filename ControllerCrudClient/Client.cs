bce1ad2a36343af168b733b6e73d44c5127dbcf3
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-08-30 13:15:50 -0300

    Create/Change  Decorators [Microsoft.AspNetCore.Mvc] - Api Controller, Route, HTTP, Produces e Consumes

--- ControllerCrudClient/Client.cs
+++ ControllerCrudClient/Client.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ControllerCrudClient
 {
     public class Client
     {
+        [Required(ErrorMessage = "CPF é obrigatório!")]
         public string cpf { get; set; }
 
+        [MaxLength(100, ErrorMessage = "O nome é muito grande, ele só pode ter 100 caracteres")]
+        [MinLength(3, ErrorMessage = "O nome é muito pequeno, ele tem que ter 3 caracteres")]
         public string name { get; set; }
 
+        [Required(ErrorMessage = "A data de nascimento é obrigatória!")]
         public DateTime birthDate { get; set; }
 
         public int age => DateTime.Now.DayOfYear < birthDate.DayOfYear ?
--- ControllerCrudClient/Controllers/ClientController.cs
+++ ControllerCrudClient/Controllers/ClientController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace ControllerCrudClient.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class ClientController : ControllerBase
     {
         string[] names = new[] { "Marcelo", "Juliana", "Nathan", "Thayssa" };
@@ -18,15 +19,18 @@ namespace ControllerCrudClient.Controllers
                 cpf = String.Concat(Enumerable.Repeat(index + 1, 9)),
                 name = names[index] + " " + lastnames[index],
                 birthDate = DateTime.Now.AddYears(-(index + 1) * 5)
+
             })
             .ToList();
         }
 
         [HttpPost]
-        public Client Create(Client client)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Client> Create(Client client)
         {
             clients.Add(client);
-            return client;
+            return CreatedAtAction(nameof(Create), client);
         }
         /* TEST CREATE CLIENT
         {
@@ -36,27 +40,39 @@ namespace ControllerCrudClient.Controllers
         }
         */
 
-        [HttpGet]
-        public IEnumerable<Client> Read()
+        [HttpGet("/clients")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Client>> Read()
         {
-            return clients;
+            return Ok(clients);
         }
 
-        [HttpPut]
-
-        public Client Update(int index, Client client)
+        [HttpPut("/{index}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Update(int index, Client client)
         {
+            if (index < 0 || index > 4)
+            {
+                return BadRequest();
+            }
             clients[index] = client;
-            return clients[index];
-
+            return NoContent();
         }
 
-        [HttpDelete]
-
-        public List<Client> Delete(int index)
+        [HttpDelete("/{index}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<List<Client>> Delete(int index)
         {
+            if ((index < 0 || index > 4))
+            {
+                return BadRequest();
+            }
             clients.RemoveAt(index);
-            return clients;
+            return Ok(clients);
+            //return StatusCode (200, clients);
+
         }
 
 
